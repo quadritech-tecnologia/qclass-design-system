a5206cb6175ecd4f08cf85e8a6b80ce208711417
Gabriel Costa <gabriel.costa@quadritech.com.br>
2024-10-14 01:22:50 -0300

    feat: create bottom sheet widget

---
+++ example_app/lib/presentation/widgets/bottom_sheet/custom_bottom_sheet.dart
@@ -0,0 +1,37 @@
+import 'package:design_system_qclass/design_system_qclass.dart';
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.UseCase(name: 'ShowDatePicker', type: CustomBottomSheet)
+Widget showBottomSheet(BuildContext context) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      Center(
+        child: CustomButton.text(
+          text: 'Mostrar bottomSheet',
+          backgroundColor: context.colorScheme.tertiary,
+          onPressed: () async {
+            await CustomBottomSheet.showBottomSheet(
+              context: context,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'testando',
+                    style: context.textTheme.titleSmall,
+                  ),
+                  CustomButton.text(
+                    text: 'Confrimar',
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    ],
+  );
+}
